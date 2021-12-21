dcb2f1fa0c842be135e67fc48d2b87a9c011bed3
bernardo <bernardomny@hotmail.com>
2021-12-20 22:05:10 -0300

    atualizando arquivos

--- main.cpp
+++ main.cpp
@@ -8,28 +8,14 @@
 #include "moduloteste.h"
 #include "TikTokData.h"
 #include "Sorts.h"
+#include "FileReader.h"
 
 using namespace std;
 
-void printArray(int a[], int n) {
-    cout << "{";
-    for (int i = 0; i < n; i++) {
-        if (i == n - 1) {
-            cout << a[i] << "}" << endl;
-        }
-        else {
-            cout << a[i] << ", ";
-        }
-    }
-}
-
 int main()
 {
-    // Doesn't work for negative numbers
-    int arr[] = {34, 23, 122, 9};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    Sorts c(n);
+
+    //Sorts c(n);
     std::vector<TikTokData> dados;
-    //s.sleepSort(arr, n);
     return 0;
 }
