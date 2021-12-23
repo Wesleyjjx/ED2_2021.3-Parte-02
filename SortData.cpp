5cd162d868e0560e53963e16a4d3c9b546cb0c1a
bernardo <bernardomny@hotmail.com>
2021-12-23 16:52:04 -0300

    atualizando arquivos

--- SortData.cpp
+++ SortData.cpp
@@ -4,13 +4,20 @@
 #include <vector>
 #include <chrono>
 
-SortData::SortData(int x, int sizeData, string sortName) {
+SortData::SortData(int x, int sizeData) {
+
     this->moveCount = 0;
+
     this->compCount = 0;
+
     this->totalTime = 0;
-    this->sortName = sortName;
+
+    this->sortName = " ";
+
     for(int i = 0; i < x ;i++) {
+
         this->keyVector.push_back(rand() % sizeData);
+
     }
 }
 
@@ -18,43 +25,75 @@ SortData::~SortData() {
 
 }
 
+string SortData::getSortName() {
+
+    return this->sortName;
+
+}
+
 int SortData::getMoveCount() {
+
     return this->moveCount;
+
 }
 
 int SortData::getCompCount() {
+
     return this->compCount;
+
 }
 
 chrono::time_point<chrono::high_resolution_clock> SortData::getBeginTime() {
+
     return this->beginTime;
+
 }
 chrono::time_point<chrono::high_resolution_clock> SortData::getEndTime() {
+
     return this->endTime;
+
 }
 
 float SortData::getTotalTime() {
+
     return this->totalTime;
+
 }
 
 void SortData::setMoveCount(int moveNumber) {
+
     this->moveCount = moveNumber;
+
 }
 
 void SortData::setCompCount(int compNumber) {
+
     this->compCount = compNumber;
+
+}
+
+void SortData::setSortName(string name) {
+
+    this->sortName = name;
+
 }
 
 void SortData::setBeginTime(chrono::time_point<chrono::high_resolution_clock> start) {
+
     this->beginTime = start;
+
 }
 
 void SortData::setEndTime(chrono::time_point<chrono::high_resolution_clock> endd) {
+
  this->endTime = endd;
+
 }
 
 void SortData::setTotalime(double total) {
+
     this->totalTime = total;
+
 }
 
 void SortData::addMoveCount() {
--- moduloteste.cpp
+++ moduloteste.cpp
@@ -14,68 +14,108 @@ moduloteste::~moduloteste()
 {
 };
 
-void moduloteste::acessaRegistro(int i, vector<TikTokData> dados)
-{
+void moduloteste::acessaRegistro(int i, vector<TikTokData> dados) {
+
     cout << "Id: " << dados.at(i).getId() << endl;
+
     cout << "Review: " << dados.at(i).getReview() << endl;
+
     cout << "UpVotes: " << dados.at(i).getUpVotes() << endl;
+
     cout << "AppVersion: " << dados.at(i).getAppVersion() << endl;
+
     cout << "PostDate: " << dados.at(i).getPostDate() << endl;
+
 };
 
-void moduloteste::testeImportacao(vector<TikTokData> dados)
-{
+void moduloteste::testeImportacao(vector<TikTokData> dados) {
     int i, couti;
 
-    std::cout << "[1] exibir saida" << std::endl << "[2] salvar em arquivo texto" << std::endl ;
+    std::cout << "[1] exibir saida" << std::endl << "[2] salvar em arquivo texto" << std::endl;
+
     std::cin >> i;
 
-    if(i == 1)
-    {
+    if(i == 1) {
+
         int ale;
 
-        for(couti = 0; couti < 10; couti++)
-        {
+        for(couti = 0; couti < 10; couti++) {
+
            srand (time(NULL));
+
            ale = rand() % 3500000;
+
            std::cout << "Registro " << couti + 1 << ": " << std::endl;
+
            std::cout << "Id: " << dados.at(i).getId() << std::endl;
+
            std::cout << "Review: " << dados.at(i).getReview() << std::endl;
+
            std::cout << "UpVotes: " << dados.at(i).getUpVotes() << std::endl;
+
            std::cout << "AppVersion: " << dados.at(i).getAppVersion() << std::endl;
+
            std::cout << "PostDate: " << dados.at(i).getPostDate() << std::endl;
+
         }
 
     }
-    else if(i == 2)
-    {
+    else if(i == 2) {
+
         int ale;
+
         ofstream wr;
+
         wr.open("data.bin",ios::binary);
-        if(!wr.is_open())
-        {
+
+        if(!wr.is_open()) {
+
             cout << "ERRO:Arquivo nao aberto." << endl;
+
             exit(1);
+
         }
 
-        for(couti = 0; couti < 100; couti++)
-        {
+        for(couti = 0; couti < 100; couti++) {
+
             srand (time(NULL));
+
             ale = rand() % 3500000;
+
             vector<TikTokData> text;
+
             text.at(couti).setId(dados.at(ale).getId());
+
             text.at(couti).setReview(dados.at(ale).getReview());
+
             text.at(couti).setUpVotes(dados.at(ale).getUpVotes());
+
             text.at(couti).setAppVersion(dados.at(ale).getAppVersion());
+
             text.at(couti).setPostDate(dados.at(ale).getPostDate());
+
 
             wr.write((char *) &text.at(couti), sizeof(text.at(couti)));
         }
+
         wr.close();
+
     }
-    else
-    {
+
+    else {
+
         cout << "ERRO:Operacao invalida."<< endl;
+
         exit(1);
+
     }
+
 };
+
+void moduloteste::randomImport(vector<TikTokData> data, vector <TikTokData> vectorRandom, int n) {
+
+    for(int i = 0; i < n; i++) {
+        vectorRandom[i] = data[rand() % 35000000];
+    }
+
+}
